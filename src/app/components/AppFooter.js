18547ecdaa70160749a63dc65d07c41fe35b5dd5
Sachin C Kasar <cse.sck@gmail.com>
2018-01-16 12:36:06 +0530

    Header and Footer
    Header and Footer

---
+++ src/app/components/AppFooter.js
@@ -0,0 +1,17 @@
+import React from "react";
+ 
+
+// AppFooter as a Class Component
+export class AppFooter extends React.Component {
+    render() {
+        return ( 
+             
+            <div className="alert alert-success" style={{textAlign:'center'}}>
+                    <div>
+                        <h3>Application Footer</h3>
+                    </div>
+                </div>
+             
+        );
+    } 
+}
---
+++ src/app/components/AppHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+ 
+
+// AppHeader as a Class Component
+export class AppHeader extends React.Component {
+    render() {
+        return ( 
+             
+                <div className="alert alert-success" style={{textAlign:'center'}}>
+                    <div>
+                        <h3>Application Header</h3>
+                    </div>
+                </div>
+             
+        );
+    } 
+}
