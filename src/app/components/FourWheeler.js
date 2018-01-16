d0e9fd0081bd6e54303b6e8b4e122b1ee5bd2e0e
Sachin C Kasar <cse.sck@gmail.com>
2018-01-16 13:24:33 +0530

    Updated
    Updated

--- src/app/components/FourWheeler.js
+++ src/app/components/FourWheeler.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 // FourWheel as a Functional Component
-export const FourWheeler = (props) => {
+export const FourWheeler = () => {
     return ( 
         <div  className="alert alert-warning" style={{border: '1px solid grey'}}>
 		  
 					<h1 style={{color:'red' , margin:'5px 5px 5px 5px'}}> Four Wheeler </h1>
 					<ul>
 						<li>
-							<b>Name :</b>  {props.name}
+							<b>Name :</b>  Grand Vitara
 						</li>
 						<li>
-							<b>By :</b>  {props.specs.manufacturer}
+							<b>By :</b>  Maruti Suzuki
 						</li>
 					</ul>
 				</div>
