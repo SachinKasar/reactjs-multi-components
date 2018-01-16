d0db4fe2f2a5f16c030036dc468167cc3cac46b3
Sachin C Kasar <cse.sck@gmail.com>
2018-01-16 12:33:01 +0530

    Updated
    Updated

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,9 @@ import {render} from "react-dom";
 
 import {TwoWheeler} from "./components/TwoWheeler";
 import {FourWheeler} from "./components/FourWheeler";
+import {AppHeader} from "./components/AppHeader";
+import {AppFooter} from "./components/AppFooter";
+
 
 class App extends React.Component {
     render() {
@@ -10,22 +13,37 @@ class App extends React.Component {
 		let fourWheelSpecs = {manufacturer : "Maruti Suzuki"};
 		
         return ( 
-			<div > <br/>
-				 <div className="container"  >
-					<div className="row">
-						<div className="row">
-							<TwoWheeler name="Enfield Classic" specs={twoWheelSpecs}/>
-						</div>
-					</div>
-				  </div>
-			      <br/>
-				  <div className="container" >
-					<div className="row">
-						<div className="row">
-							<FourWheeler name="Grand Vitara" specs={fourWheelSpecs}/>
-						</div>
-					</div>
-				  </div>
+			 <div className="container"  >
+            
+               <div className="row">
+                      <div className="col-xs-10 col-xs-offset-1" >
+                         <AppHeader/>
+                      </div>
+                </div>
+				    
+            <div className="row col-xs-10 col-xs-offset-1" style={{border: '1px solid grey'}} >
+            <br/>   
+            <div className="row">
+                      <div className="col-xs-10 col-xs-offset-1" >
+                         <TwoWheeler name="Enfield Classic" specs={twoWheelSpecs}/>
+                      </div>
+                </div>
+				   
+              <br/>
+            
+              <div className="row">
+                  <div className="col-xs-10 col-xs-offset-1">
+                        <FourWheeler name="Grand Vitara" specs={fourWheelSpecs}/>
+                   </div>
+               </div>
+            </div>
+            
+             <div className="row" style={{marginTop: '350px'}}>
+                      <div className="col-xs-10 col-xs-offset-1" >
+                         <AppFooter/>
+                      </div>
+                </div>
+				   
 			  </div>
         );
     } 
